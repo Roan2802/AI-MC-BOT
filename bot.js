c51fd238284fda5809e950c8755c8d8d8be7c980
Roan2802 <Roanvanasselt@gmail.com>
2025-11-16 12:58:05 +0000

    comit

--- bot.js
+++ bot.js
@@ -2,12 +2,12 @@ import mineflayer from 'mineflayer'
 import { pathfinder } from 'mineflayer-pathfinder'
 
 const bot = mineflayer.createBot({
-  host: '192.168.1.219', // jouw LAN-server IP
-  port: 30000,           // jouw aangepaste poort
+  host: 'localhost', // werkt ook via LAN
+  port: 30000,
   username: 'Agent01'
 })
 
 bot.once('spawn', () => {
   bot.loadPlugin(pathfinder)
-  console.log('Agent01 is online and ready on LAN!')
+  console.log('Agent01 is online in the world!')
 })
